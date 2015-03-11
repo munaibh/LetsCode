5e6e3b21a74ea7d00b72fde5b162bfea1aeee1e5
munaibh <hussain123>
2015-03-11 16:36:57 +0000

    Added Header to JFrame
    I have added the header to the JFrame, within the header I have added a
    series of buttons aswell as the logo for the project. These JButtons
    will become system controls.

--- LetsCode/src/MainGUI.java
+++ LetsCode/src/MainGUI.java
@@ -4,18 +4,26 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
 
 
 
 public class MainGUI implements ActionListener {
 	
 	private JFrame frame;
-	private JPanel panel;
+	private JLabel logo;
+	private JPanel panel, header, wrapper, backnext, settings, controls;
+	private JButton exitBtn, miniBtn, maxiBtn, nextBtn, backBtn, settBtn;
 
 	public MainGUI() {
 		
@@ -31,10 +39,54 @@ public class MainGUI implements ActionListener {
 		frame = new JFrame();
 		panel = new JPanel();
 		
+    	header   = new JPanel();
+		wrapper  = new JPanel();
+		backnext = new JPanel();
+    	controls = new JPanel();
+    	settings = new JPanel();
+    	
+    	backBtn  = new JButton();
+    	nextBtn  = new JButton();	
+    	settBtn  = new JButton();
+    	maxiBtn  = new JButton();
+    	exitBtn  = new JButton();
+    	miniBtn  = new JButton();
+    	
+    	exitBtn.addActionListener(this);
+    	miniBtn.addActionListener(this);
+    	
+    	panel.setLayout(new MigLayout("insets 0"));
+    	header.setLayout(new MigLayout("insets 15, gap 0"));
+    	backnext.setLayout(new MigLayout("insets 0, gap 0"));
+    	controls.setLayout(new MigLayout("insets 0, gap 0"));
+    	settings.setLayout(new MigLayout("insets 0, gap 0"));
+
+		
 	}
 	
 	
 	public void addElements() {
+		
+		
+		backnext.add(backBtn);
+    	backnext.add(nextBtn);
+ 
+    	settings.add(settBtn, "right, push, gap 10 10");
+    	controls.add(maxiBtn, "right, push");
+    	controls.add(miniBtn, "right");
+    	controls.add(exitBtn, "right");
+    	
+    	header.add(backnext, "w 200px");
+    	header.add(logo,     "push, growx");
+    	header.add(settings, "w 73px, gap 10 10");
+    	header.add(controls, "w 60px");
+		
+    	panel.add(header, "growx, hmax 90px, top, wrap");
+		panel.add(wrapper, "push, grow, top");
+
+		
+		panel.add(header, "growx, hmax 90px, top, wrap");
+		panel.add(wrapper, "push, grow, top");
 		
 		frame.setUndecorated(true);
 		frame.setContentPane(new ShadowPane());
@@ -50,7 +102,23 @@ public class MainGUI implements ActionListener {
 	
 	public void styleElements() {
 		
+		// Add Logo
+		
+
+    	try {
+            Image image = ImageIO.read(new File("assets/img/logo.png"));
+			Image  resized  = image.getScaledInstance(55, 55,Image.SCALE_SMOOTH);
+            ImageIcon logoImage = new ImageIcon(resized);
+    		logo = new JLabel(logoImage);
+    		
+        } catch (IOException k) { k.printStackTrace(); }
+    	
+    	
+    	header.setBackground(Color.WHITE);
     	panel.setBackground(new Color(0xf8f8f8));
+    	wrapper.setBackground(new Color(0,0,0,0));
+		header.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(0xeef1f1)));
+
 		
 	}
 
